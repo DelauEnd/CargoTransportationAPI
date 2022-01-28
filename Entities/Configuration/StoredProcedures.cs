68b7c5501e62c7a800ecee850654b6105e0abd2c
Дима Савицкий <2sabba322@gmail.com>
2022-01-28 15:48:04 +0300

    changed procedure string, deleted unnecessary concatenations
    closes #6

--- Entities/Configuration/StoredProcedures.cs
+++ Entities/Configuration/StoredProcedures.cs
@@ -7,14 +7,14 @@ namespace Entities.Configuration
     public class StoredProcedures
     {
         public static string AssignCargoToRoute =>
-            "create procedure AssignCargoToRoute " +
-            "@cargoId int, " +
-            "@routeId nvarchar(10) " +
-            "as " +
-            "begin " +
-            "update Cargoes " +
-            "set RouteId = @routeId " +
-            "where CargoId = @cargoId " +
-            "end";
+            @"create procedure AssignCargoToRoute
+            @cargoId int, 
+            @routeId nvarchar(10)
+            as 
+            begin 
+            update Cargoes 
+            set RouteId = @routeId 
+            where CargoId = @cargoId
+            end";
     }
 }
